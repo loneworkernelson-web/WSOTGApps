16139c2e6153f093704b2fd0f5031531d218b9a8
loneworkernelson-web <loneworkernelson@gmail.com>
2025-12-05 13:58:55 +1300

    Add files via upload

---
+++ WorkerApp/sw.js
@@ -0,0 +1,1 @@
+const C='v16.0';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./index.html']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
