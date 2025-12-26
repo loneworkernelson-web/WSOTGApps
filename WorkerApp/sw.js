f40f72208d70e63a1e256ecba1d7035d22c589a8
loneworkernelson-web <loneworkernelson@gmail.com>
2025-12-26 19:47:17 +1300

    Delete WorkerApp directory

--- WorkerApp/sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE_NAME='v79.4_1766540346884';const u=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(u)))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>{return r||fetch(e.request)}))});
